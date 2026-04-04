beb9d4916dae0072b44588dc9567d5520f6ce641
Beenathyn <bhove@wpi.edu>
2026-04-03 22:28:39 -0400

    Add files via upload

---
+++ sprout.js
@@ -0,0 +1,16 @@
+   
+   $(document).ready(function()
+   {
+      $("a[data-rel='PhotoGallery1']").attr('rel', 'PhotoGallery1');
+      $("#PhotoGallery1").magnificPopup({delegate:'a', type:'image', zoom: { enabled: true, duration: 300, easing: 'ease-in-out' }, gallery: {enabled: true, navigateByImgClick: true}});
+      $("a[data-rel='PhotoGallery2']").attr('rel', 'PhotoGallery2');
+      $("#PhotoGallery2").magnificPopup({delegate:'a', type:'image', zoom: { enabled: true, duration: 300, easing: 'ease-in-out' }, gallery: {enabled: true, navigateByImgClick: true}});
+      $("a[data-rel='PhotoGallery3']").attr('rel', 'PhotoGallery3');
+      $("#PhotoGallery3").magnificPopup({delegate:'a', type:'image', zoom: { enabled: true, duration: 300, easing: 'ease-in-out' }, gallery: {enabled: true, navigateByImgClick: true}});
+      $("a[data-rel='PhotoGallery4']").attr('rel', 'PhotoGallery4');
+      $("#PhotoGallery4").magnificPopup({delegate:'a', type:'image', zoom: { enabled: true, duration: 300, easing: 'ease-in-out' }, gallery: {enabled: true, navigateByImgClick: true}});
+      $("a[data-rel='PhotoGallery5']").attr('rel', 'PhotoGallery5');
+      $("#PhotoGallery5").magnificPopup({delegate:'a', type:'image', zoom: { enabled: true, duration: 300, easing: 'ease-in-out' }, gallery: {enabled: true, navigateByImgClick: true}});
+      $("a[data-rel='PhotoGallery6']").attr('rel', 'PhotoGallery6');
+      $("#PhotoGallery6").magnificPopup({delegate:'a', type:'image', zoom: { enabled: true, duration: 300, easing: 'ease-in-out' }, gallery: {enabled: true, navigateByImgClick: true}});
+   });
