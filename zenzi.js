66be0731d285bfc38c910c3a78df73351ffe6d6a
Beenathyn <bhove@wpi.edu>
2026-04-03 22:21:31 -0400

    Delete zenzi.js

--- zenzi.js
+++
@@ -1,6 +0,0 @@
-   
-   $(document).ready(function()
-   {
-      $("a[data-rel='PhotoGallery1']").attr('rel', 'PhotoGallery1');
-      $("#PhotoGallery1").magnificPopup({delegate:'a', type:'image', zoom: { enabled: true, duration: 300, easing: 'ease-in-out' }, gallery: {enabled: true, navigateByImgClick: true}});
-   });
